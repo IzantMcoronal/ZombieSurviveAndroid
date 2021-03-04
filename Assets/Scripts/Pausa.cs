0250c080f0f386dafef2cb697e7eda2b0744a91e
ManuC <56949815+IzantManuC@users.noreply.github.com>
2021-03-04 20:50:49 +0100

    Version Android is Ok

---
+++ Assets/Scripts/Pausa.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pausa : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenu;
+
+    void Update(){
+
+    }
+
+    public void Resume(){
+
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+
+    }
+
+    public void Pause(){
+
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+
+   }
+}
+
